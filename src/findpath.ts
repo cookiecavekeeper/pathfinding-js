234e4baf1c7f8db6039b79bc8d96dfa90bf838f8
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2017-09-22 20:33:08 +0200

    use stricter ts checking

--- src/findpath.ts
+++ src/findpath.ts
@@ -1,9 +1,37 @@
 import * as readline from 'readline';
 
+main();
 
+async function main() {
+    let input = await readInput();
+    console.log(input);
+}
 
-let reader = readline.createInterface(process.stdin);
+interface Input {
+    rows: number
+    data: string[][]
+}
 
-reader.on('line', line => {
-    console.log(line);
-});
+function readInput(): Promise<Input> {
+    return new Promise((resolve) => {
+        let reader = readline.createInterface(process.stdin);
+        let firstLine = true;
+
+        let rows: number;
+
+        let data: string[][] = [];
+
+        reader.on('line', (line: string) => {
+            if (firstLine) {
+                rows = +line;
+                firstLine = false;
+            } else {
+                data.push(line.split(''));
+            }
+        });
+
+        reader.on('close', () => {
+            resolve({rows, data});
+        });
+    });
+}
